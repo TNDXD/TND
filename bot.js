41cc21eac7e067b12a5a609b71fc639345454dfa
TNDXD <48101705+TNDXD@users.noreply.github.com>
2019-07-07 06:56:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -385,7 +385,7 @@ client.on("message", message => {
   const embed = new Discord.RichEmbed()
       .setColor("RANDOM")
       .setFooter('عيون خالد')  
-  message.channel.sendembed(embed);
+  message.channel.sendEmbed(embed);
 
  }
 });
