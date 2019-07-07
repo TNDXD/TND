3bf8bd99072891c49fcf0568f5958992856ccdc0
TNDXD <48101705+TNDXD@users.noreply.github.com>
2019-07-07 11:14:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -455,6 +455,49 @@ client.on("message", message => {
 
 
 
+client.on("message", message => {
+ if (message.content === "تسجيل دخول") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('✅ تم تسجيل دخولك')  
+  message.channel.sendEmbed(embed);
+
+ }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on("message", message => {
+ if (message.content === "تسجيل خروج") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('✅ تم تسجيل خروجك')  
+  message.channel.sendEmbed(embed);
+
+ }
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 
